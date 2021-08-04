1fb195916d8af70ec66befd7c5925537ba5a4f28
Dayanne Chaves <dayannemdchaves@gmail.com>
2021-08-04 16:27:27 -0300

    corrigindo Questão6

--- Questao6.java
+++ Questao6.java
@@ -7,26 +7,28 @@ public class Questao6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/* 6 - Escrever um programa que receba varios numeros inteiros no teclado. E no final imprimir a media dos numeros multiplos de 3. Para sair digitar 0(zero).(DO...WHILE)*/
+		//6 - Escrever um programa que receba varios numeros inteiros no teclado. E no final imprimir a media dos numeros multiplos de 3. Para sair digitar 0(zero).(DO...WHILE)
 		
-		int numero,media,contador=0,soma=0;
+		int numero,media=0,contador=0,soma=0;
 		
 		Scanner leia=new Scanner(System.in);
 		
 		do
 		{
-			System.out.println("Entre com um número de 1 a 9 ou digite 0 para sair: ");
+			System.out.println("Entre com um número ou digite 0 para sair: ");
 			numero=leia.nextInt();
 			
-			if(numero%3==0 && numero!=0)
+			if(numero!=0)
 			{
-				soma+=numero;
-				contador++;
+				if(numero%3==0)
+				{
+					soma+=numero;
+					contador++;
+				}
+				media=soma/contador;
 			}
 		}
 		while(numero!=0);
-		
-		media=soma/contador;
 		
 		System.out.println("A media dos números múltiplos de 3 digitados é: "+media);
 
